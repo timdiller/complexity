605e06c0f8becb2739ae5b8bd61c340cecc48305
Tim Diller <timdiller@gmail.com>
2013-07-17 20:32:17 -0500

    add time series plots for fire and tree density

--- forest.py
+++ forest.py
@@ -9,6 +9,8 @@ from traits.api import (HasTraits, Array, Bool, Button, DelegatesTo,  # Float,
                         Instance, Int, Property, Range, String)
 from traitsui.api import ButtonEditor, Item, RangeEditor, View
 
+history_length = 500
+
 
 class Forest(HasTraits):
     p_lightning = Range(0., 1., 0.01)
@@ -66,6 +68,10 @@ class ForestView(HasTraits):
     p_sapling = DelegatesTo("forest", "p_sapling")
     p_lightning = DelegatesTo("forest", "p_lightning")
     forest_plot = Instance(Plot)
+    time_plot = Instance(Plot)
+    ratio_history = Array(dtype=float)
+    fire_history = Array(dtype=float)
+    time = Array(dtype=int)
     plot_data = Instance(ArrayPlotData)
     run_label = Property(String, depends_on="run")
     run_button = Button
@@ -73,6 +79,7 @@ class ForestView(HasTraits):
 
     traits_view = View(
         Item("forest_plot", editor=ComponentEditor(), show_label=False),
+        Item("time_plot", editor=ComponentEditor(), show_label=False),
         Item("p_sapling", editor=RangeEditor(), label="p sapling"),
         Item("p_lightning", editor=RangeEditor(), label="p lightning"),
         Item("run_button", editor=ButtonEditor(label_value="run_label"),
@@ -85,10 +92,30 @@ class ForestView(HasTraits):
         self._advance()
         event.mark_as_handled()
 
+    def update_fire_history(self):
+        self.fire_history[1:] = self.fire_history[:-1]
+        self.fire_history[0] = float(np.sum(self.forest.forest_fires)) / \
+            self.forest.forest_fires.size
+
+    def update_ratio_history(self):
+        self.ratio_history[1:] = self.ratio_history[:-1]
+        self.ratio_history[0] = float(np.sum(self.forest.forest_grid)) / \
+            self.forest.forest_grid.size
+
+    def update_time(self):
+        self.time[1:] = self.time[:-1]
+        self.time[0] = self.time[1] + 1
+
     def _advance(self):
         self.forest.advance_one_day()
+        self.update_fire_history()
+        self.update_ratio_history()
+        self.update_time()
         self.plot_data.set_data("forest_grid", self.forest.forest_grid +
                                 2 * self.forest.forest_fires)
+        self.plot_data.set_data("fire_history", self.fire_history)
+        self.plot_data.set_data("ratio_history", self.ratio_history)
+        self.plot_data.set_data("time", self.time)
 
     def _day_fired(self):
         self._advance()
@@ -96,6 +123,9 @@ class ForestView(HasTraits):
     def _em_default(self):
         em = EventManager()
         return em
+
+    def _fire_history_default(self):
+        return np.zeros((history_length, ), dtype=float)
 
     def _get_run_label(self):
         if self.run:
@@ -106,6 +136,9 @@ class ForestView(HasTraits):
 
     def _hb_default(self):
         return Heartbeat(interval=0.05, event_manager=self.em)
+
+    def _ratio_history_default(self):
+        return np.zeros((history_length, ), dtype=float)
 
     def _run_button_fired(self):
         if self.run:
@@ -125,16 +158,28 @@ class ForestView(HasTraits):
         self.hb.serve()
         return False
 
+    def _plot_data_default(self):
+        forest_grid = np.asarray(self.forest.forest_grid, dtype=int)
+        data = ArrayPlotData(forest_grid=forest_grid,
+                             ratio_history=self.ratio_history,
+                             fire_history=self.fire_history,
+                             time=self.time)
+        return data
+
     def _forest_plot_default(self):
         plot = Plot(self.plot_data)
         plot.img_plot("forest_grid")
         plot.bounds = [0., 2.0]
         return plot
 
-    def _plot_data_default(self):
-        forest_grid = np.asarray(self.forest.forest_grid, dtype=int)
-        data = ArrayPlotData(forest_grid=forest_grid)
-        return data
+    def _time_plot_default(self):
+        plot = Plot(self.plot_data)
+        plot.plot(["time", "ratio_history"])
+        plot.plot(["time", "fire_history"])
+        return plot
+
+    def _time_default(self):
+        return np.zeros((history_length, ), dtype=int)
 
 
 if __name__ == "__main__":
